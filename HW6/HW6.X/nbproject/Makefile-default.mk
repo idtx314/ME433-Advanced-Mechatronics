0565dcad6ef5d7bd6a016ec2d5daf7cbcb3bc934
Ian Davis <idtx314business@gmail.com>
2018-04-30 14:34:58 -0500

    Added new libraries for SPI1 and LCD to HW6. Deleted older functions related to SPI tasks.

--- HW6/HW6.X/nbproject/Makefile-default.mk
+++ HW6/HW6.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=main.c i2clib.c
+SOURCEFILES_QUOTED_IF_SPACED=main.c i2clib.c ST7735.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.o ${OBJECTDIR}/i2clib.o
-POSSIBLE_DEPFILES=${OBJECTDIR}/main.o.d ${OBJECTDIR}/i2clib.o.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.o ${OBJECTDIR}/i2clib.o ${OBJECTDIR}/ST7735.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/main.o.d ${OBJECTDIR}/i2clib.o.d ${OBJECTDIR}/ST7735.o.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/main.o ${OBJECTDIR}/i2clib.o
+OBJECTFILES=${OBJECTDIR}/main.o ${OBJECTDIR}/i2clib.o ${OBJECTDIR}/ST7735.o
 
 # Source Files
-SOURCEFILES=main.c i2clib.c
+SOURCEFILES=main.c i2clib.c ST7735.c
 
 
 CFLAGS=
@@ -118,6 +118,12 @@ ${OBJECTDIR}/i2clib.o: i2clib.c  nbproject/Makefile-${CND_CONF}.mk
 	@${RM} ${OBJECTDIR}/i2clib.o 
 	@${FIXDEPS} "${OBJECTDIR}/i2clib.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -DPICkit3PlatformTool=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/i2clib.o.d" -o ${OBJECTDIR}/i2clib.o i2clib.c    -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD) 
 	
+${OBJECTDIR}/ST7735.o: ST7735.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/ST7735.o.d 
+	@${RM} ${OBJECTDIR}/ST7735.o 
+	@${FIXDEPS} "${OBJECTDIR}/ST7735.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -DPICkit3PlatformTool=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/ST7735.o.d" -o ${OBJECTDIR}/ST7735.o ST7735.c    -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD) 
+	
 else
 ${OBJECTDIR}/main.o: main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
@@ -130,6 +136,12 @@ ${OBJECTDIR}/i2clib.o: i2clib.c  nbproject/Makefile-${CND_CONF}.mk
 	@${RM} ${OBJECTDIR}/i2clib.o.d 
 	@${RM} ${OBJECTDIR}/i2clib.o 
 	@${FIXDEPS} "${OBJECTDIR}/i2clib.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/i2clib.o.d" -o ${OBJECTDIR}/i2clib.o i2clib.c    -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD) 
+	
+${OBJECTDIR}/ST7735.o: ST7735.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/ST7735.o.d 
+	@${RM} ${OBJECTDIR}/ST7735.o 
+	@${FIXDEPS} "${OBJECTDIR}/ST7735.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/ST7735.o.d" -o ${OBJECTDIR}/ST7735.o ST7735.c    -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD) 
 	
 endif
 
