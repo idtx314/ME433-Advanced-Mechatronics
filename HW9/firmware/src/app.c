a89ff178738b6224dc59cb1395838811e2f06df8
Ian Davis <idtx314business@gmail.com>
2018-05-09 17:59:38 -0500

    Z axis accel data transmitting at 100Hz. Now setting it up to read at 5Hz while still transmitting at 100Hz. Not clear on why this was assigned.

--- HW9/firmware/src/app.c
+++ HW9/firmware/src/app.c
@@ -466,7 +466,7 @@ void APP_Tasks(void) {      //Setup is such that the switch is only called when 
              * The isReadComplete flag gets updated in the CDC event handler. */
 
              /* WAIT FOR 5HZ TO PASS OR UNTIL A LETTER IS RECEIVED */
-            if (appData.isReadComplete || _CP0_GET_COUNT() - startTime > (48000000 / 2 / 5)) {
+            if (appData.isReadComplete || _CP0_GET_COUNT() - startTime > (48000000 / 2 / 100)) {
                 appData.state = APP_STATE_SCHEDULE_WRITE;
             }
 
