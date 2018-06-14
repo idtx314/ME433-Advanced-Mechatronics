193e4210a2ab9bb740b1a16f1c666694a42fc0ed
Ian Davis <idtx314business@gmail.com>
2018-06-13 19:02:53 -0500

    HW16 code is tested to the extent that it causes the motors to move. I will need to make a couple of wiring adjustments, but assembly of the first prototype has gone pretty well. Added a video of the complete prototype and the motors responding to desired velocity of 10.

--- HW16/firmware/src/app.c
+++ HW16/firmware/src/app.c
@@ -389,7 +389,7 @@ void APP_Initialize(void) {
 
     startTime = _CP0_GET_COUNT();
     
-    l_d_vel = r_d_vel = 1;
+    l_d_vel = r_d_vel = 10;
 }
 
 /******************************************************************************
