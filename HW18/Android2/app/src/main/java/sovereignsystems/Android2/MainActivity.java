6094b83258000d5c3ebe0382fe4d72d47badd4e6
Ian Davis <idtx314business@gmail.com>
2018-08-27 20:27:17 -0500

    Alterations to activity_main.xml to prep for new features. Tested and working with PIC.

--- HW18/Android2/app/src/main/java/sovereignsystems/Android2/MainActivity.java
+++ HW18/Android2/app/src/main/java/sovereignsystems/Android2/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myControl = (SeekBar) findViewById(R.id.seek1);
+        myControl = (SeekBar) findViewById(R.id.CoMseek);
 
         myTextView = (TextView) findViewById(R.id.textView01);
         myTextView.setText("Enter whatever you Like!");
